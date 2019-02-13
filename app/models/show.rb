aaf2f0f40b240a6f19a87a59c5745d165627582b
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-13 22:49:21 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,5 +3,9 @@ class Show < ActiveRecord::Base
     def highest_rating
       order("rating DESC").first.rating
     end
+    
+    def most_popular_show
+      highest_rating∂∂
+    end
   end
 end
